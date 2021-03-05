2902aa036f46b2e6aecd360846bd76e35cf35f7f
Uday519 <bhaskaruday39@gmail.com>
2021-03-05 13:23:14 +0530

    Updated with new Keywords

--- app/src/main/java/com/uday/hacked/HackedKeyboard.java
+++ app/src/main/java/com/uday/hacked/HackedKeyboard.java
@@ -15,6 +15,10 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static android.content.ContentValues.TAG;
 
 public class HackedKeyboard extends AccessibilityService {
@@ -23,20 +27,38 @@ public class HackedKeyboard extends AccessibilityService {
         super.onServiceConnected();
     }
 
+    List<String> keyWords = new ArrayList<>(
+            Arrays.asList("porn", "pornhub","xvideos","xhamster","lobstertube","vk","xxx"));
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         AccessibilityNodeInfo source = event.getSource();
         if (source != null & event.getClassName().equals("android.widget.EditText")) {
-            String  text_entereted = event.getText().toString();
-            text_entereted = text_entereted.replace('[', ' ');
-            text_entereted = text_entereted.replace(']', ' ');
-            text_entereted = text_entereted.trim();
-            text_entereted = text_entereted.toLowerCase();
-            if(text_entereted.equals("android")){
-                Bundle arguments = new Bundle();
-                arguments.putCharSequence(AccessibilityNodeInfo
-                        .ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "Hacked");
-                source.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
+            String  text_entereted = event.getText().toString(), finalString = "";
+            boolean matchFound = false;
+            if(text_entereted != null){
+                text_entereted = text_entereted.replace('[', ' ');
+                text_entereted = text_entereted.replace(']', ' ');
+                text_entereted = text_entereted.trim();
+                text_entereted = text_entereted.toLowerCase();
+                String [] text_split = text_entereted.split(" ");
+                Log.d("bu", text_entereted);
+                for(int i=0; i<text_split.length; i++){
+                    if(keyWords.contains(text_split[i])){
+                        finalString = finalString.concat("Restricted Keyword ");
+                        matchFound = true;
+                    }
+                    else{
+                        finalString = finalString.concat(text_split[i] + " ");
+                    }
+                }
+                if(matchFound) {
+                    Bundle arguments = new Bundle();
+                    arguments.putCharSequence(AccessibilityNodeInfo
+                            .ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, finalString);
+                    source.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
+                }
+
             }
         }
     }
@@ -45,5 +67,7 @@ public class HackedKeyboard extends AccessibilityService {
     public void onInterrupt() {
 
     }
+
+
 
 }
